4655ba86bfceab1f822f9fb3ff3643c1bda8e6a2
Adestar1 <xtaidowu@gmail.com>
2022-06-24 10:19:05 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let myName = "Esther Idowu";
+document.write("Name:" + "" + myName + "<br/>");
+
+let height = "1.6m";
+document.write("Height:" + "" + height + "<br/>");
+
+let country = "Nigeria";
+document.write(" Country:" + "" + country + "<br/>");
